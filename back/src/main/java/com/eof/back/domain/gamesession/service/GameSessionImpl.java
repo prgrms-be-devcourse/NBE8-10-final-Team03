24aae8197c876bde69c8bcbca9b99d1c792731f3
BaekRaven <yjw70809@gmail.com>
2026-03-18 16:47:24 +0900

    Feat: GameSession domain repository,service 추가 | Create관련 Dto추가

---
+++ back/src/main/java/com/eof/back/domain/gamesession/service/GameSessionImpl.java
@@ -0,0 +1,10 @@
+package com.eof.back.domain.gamesession.service;
+
+/**
+ * 게임세션(room) 인터페이스의 기본 구현체입니다.
+ *
+ * @author 유재원
+ * @since 2026-03-18
+ */
+public class GameSessionImpl {
+}
---
+++ back/src/main/java/com/eof/back/domain/gamesession/service/GameSessionService.java
@@ -0,0 +1,10 @@
+package com.eof.back.domain.gamesession.service;
+
+/**
+ * 게임세션(room)관련 비즈니스 로직을 정의하는 인터페이스입니다.
+ *
+ * @author 유재원
+ * @since 2026-03-18
+ */
+public interface GameSessionService {
+}

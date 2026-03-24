c5d3bbe3c9c25047ecbe70baa8172b9bdfcc772a
onethousand121 <onethousand121@gmail.com>
2026-03-24 15:18:26 +0900

    Refactor : userprincipal import 경로 변경

--- back/src/main/java/com/eof/back/domain/gamerecord/controller/RecordController.java
+++ back/src/main/java/com/eof/back/domain/gamerecord/controller/RecordController.java
@@ -2,13 +2,12 @@ package com.eof.back.domain.gamerecord.controller;
 
 import com.eof.back.domain.gamerecord.dto.UserRecordResponse;
 import com.eof.back.domain.gamerecord.service.RecordService;
-import com.eof.back.domain.user.dto.UserPrincipal;
+import com.eof.back.global.jwt.UserPrincipal;
 import com.eof.back.global.response.CommonResponse;
 import com.eof.back.global.response.Response;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;

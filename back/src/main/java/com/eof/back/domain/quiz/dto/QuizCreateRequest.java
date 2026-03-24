f8aa5e2afdbb0d512a3cc42b2ee9d882d6b0653e
MintyU <mintyu.dev@gmail.com>
2026-03-24 10:19:27 +0900

    Feat: Quiz Service 구현

--- back/src/main/java/com/eof/back/domain/quiz/dto/QuizCreateRequest.java
+++ back/src/main/java/com/eof/back/domain/quiz/dto/QuizCreateRequest.java
@@ -1,4 +1,11 @@
 package com.eof.back.domain.quiz.dto;
 
-public record QuizCreateRequest() {
+public record QuizCreateRequest(
+        String content,
+        String answer,
+        String choice1,
+        String choice2,
+        String choice3,
+        String choice4
+) {
 }

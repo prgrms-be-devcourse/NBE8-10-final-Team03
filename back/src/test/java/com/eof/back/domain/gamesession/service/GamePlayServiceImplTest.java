a50892ab7561cf7c95af0b2aecc310113b225d85
BaekRaven <yjw70809@gmail.com>
2026-03-27 17:38:06 +0900

    Fix : 불필요한 주석 제거

--- back/src/test/java/com/eof/back/domain/gamesession/service/GamePlayServiceImplTest.java
+++ back/src/test/java/com/eof/back/domain/gamesession/service/GamePlayServiceImplTest.java
@@ -1,33 +1,9 @@
 package com.eof.back.domain.gamesession.service;
 
-/**
- * 코드에 대한 전체적인 역할을 적습니다.
- * <p>
- * 코드에 대한 작동 원리 등을 적습니다.
- *
- * <p><b>상속 정보:</b><br>
- * 상속 정보를 적습니다.
- *
- * <p><b>주요 생성자:</b><br>
- * {@code ExampleClass(String example)}  <br>
- * 주요 생성자와 그 매개변수에 대한 설명을 적습니다. <br>
- *
- * <p><b>빈 관리:</b><br>
- * 필요 시 빈 관리에 대한 내용을 적습니다.
- *
- * <p><b>외부 모듈:</b><br>
- * 필요 시 외부 모듈에 대한 내용을 적습니다.
- *
- * @author 유재원
- * @see
- * @since 2026-03-27
- */
-
 import com.eof.back.domain.gamesession.entity.GameSession;
 import com.eof.back.domain.gamesession.repository.GameSessionRepository;
 import com.eof.back.domain.quiz.entity.Quiz;
 import com.eof.back.domain.quizset.entity.QuizSet;
-import com.eof.back.domain.user.gamerecord.service.RecordService;
 import com.eof.back.domain.user.user.entity.User;
 import com.eof.back.global.exception.exceptions.GameSessionException;
 import org.junit.jupiter.api.BeforeEach;
